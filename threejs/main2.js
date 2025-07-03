2b4a13e0bd519f2cded33d08f5a0dfab0118a774
Aidan <aidan.perez1608@gmail.com>
2025-07-03 16:53:05 -0400

    Adding instructions + organization + pauses

--- threejs/main2.js
+++ threejs/main2.js
@@ -24,9 +24,9 @@ scene.add(directionalLightHelper);
 
 let pi=3.14159
 
-let x=10
-let y=10
-let z=1
+let x=0.01
+let y=0.01
+let z=0.01
 
 //WE GOT DA BAWX
 const geometry = new THREE.BoxGeometry(x,y,z); 
@@ -40,20 +40,47 @@ camera.position.z = 100;
 
 
 
-let counter=0
-let phase1=1
+
 
 let oldcubescaleZ=0  //stores the old z-scale of object from the previous frame (its used to hold illusion of keeping the object same size)
 
-
+//The following variables are INDICATIVE OF WHEN THE PHASES END (FOR EXAMPLE: p1=1, MEANS (PHASE 1) ENDS AT counter=1)
+let counter=0
+let p1=1
+let p2=3
+let p3=5
+let p4=7
+let p5=9
 
 function animate() {
   requestAnimationFrame(animate);
   oldcubescaleZ=cube.scale.z
+
   counter+=.001
   console.log(counter)
 
+
+
+  if(counter<p1&&counter>0){
+    //oblivion to dot
+
+  }
+  if(counter<p2&&counter>p1+1){
+    //dot to rod
+  }
+  if(counter<p3&&counter>p2+1){
+    //rod to plane
+  }
+  if(counter<p4&&counter>p3+1){
+    //plane to box
+  }
+  if(counter<p5&&counter>p4+1){
+    //box to oblivion
+  }
+  if(counter>p5+1){counter=0}
   
+
+
   renderer.render(scene, camera);
 }
 
