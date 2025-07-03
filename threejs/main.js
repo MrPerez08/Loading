3cca2fe29cfa56373eac67d0fc67159c53b16952
Aidan <aidan.perez1608@gmail.com>
2025-07-03 16:10:12 -0400

    Z coord learning + lighting

--- threejs/main.js
+++ threejs/main.js
@@ -14,44 +14,55 @@ const segments = 40;
 let π=3.14159
 
 
-let geo2 = new THREE.CylinderGeometry( 5, 5, 5, 32 ); 
-geo2.center();
-const mat2 = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
-const cylinder = new THREE.Mesh( geo2, mat2 );
-cylinder.position.set(0,0,0)
-scene.add(cylinder);
-cylinder.rotation.z = π/2
+let x=10
+let y=10
+let z=.2
 
-
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
-const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
+const geometry = new THREE.BoxGeometry(x,y,z); 
+const material = new THREE.MeshStandardMaterial( {color: 0x00ff00}); 
 const cube = new THREE.Mesh( geometry, material ); 
-//scene.add( cube );
+scene.add( cube );
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // color, intensity
+scene.add(ambientLight);
 
 
 
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(-10, -20, 20);
+scene.add(directionalLight);
+
+// Optional: Add helper to visualize light
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+scene.add(directionalLightHelper);
+
+
+
+
+cube.rotation.x=π/2
 
 camera.position.z = 100;
 
+let oldcubescaleZ=0  //stores the old z-scale of object from the previous frame (its used to hold illusion of keeping the object same size)
 
 function animate() {
-    // Increase radius
+  requestAnimationFrame(animate);
+  //oldcubescaleZ=cube.scale.z
     
-    //radius += 0.005;
-    
-    // Create a new geometry with the updated radius
-    //const newGeo = new THREE.CircleGeometry(radius, segments);
-    
-    // Replace the old geometry with the new one
-    //circle.geometry.dispose(); // Important: clean up the old geometry
-    //circle.geometry = newGeo;
-    
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
-    cube.rotation.x +=.01
-    cube.rotation.y +=.01
-    cube.rotation.z +=.01
+  //cube.rotation.x +=.01
+  //cube.rotation.y +=.01
+
+
+  //One Dimensional
+
+  //Two Dimensional
+
+  //Three Dimensional
+  cube.rotation.z +=.01
+  cube.scale.z=cube.scale.x*x/z //extrudes z-coordinate of cube to turn the square into a cube
+  //camera.position.z+=z*(cube.scale.z-oldcubescaleZ)/2  /maintains percieved scale of the object to the camera
+
+  renderer.render(scene, camera);
 }
 
 animate();
